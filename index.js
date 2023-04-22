1dbdfb036536d18d5240da8470f7605df333ed71
shubham <shubham@itday.in>
2023-04-22 21:35:09 +0530

    optimised maxDiff

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const arr = [30,10,8,2]
-let maxDiff = Number.NEGATIVE_INFINITY
-for (let i = 0; i < arr.length; i++) {
-   
-    for (let j = i+1; j < arr.length; j++) {
-      let currDiff = arr[j] - arr[i]
-      if (currDiff > maxDiff) {
-        maxDiff = currDiff
-      }
-        
-    }
+const arr = [2,3,10,6,4,8,1]
+
+let minValue = arr[0]
+
+let maxDiff = arr[1] - arr[0]
+
+for (let j = 1; j < arr.length; j++) {
+    
+    maxDiff = Math.max(maxDiff,arr[j] - minValue)
+    minValue = Math.min(minValue,arr[j])
     
 }
 
