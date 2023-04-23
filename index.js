b42ca4ceb0d87d92935d8bc67766d69b4177cf17
shubham <shubham@itday.in>
2023-04-23 23:04:26 +0530

    bubblr sort

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-// binary search with sorted array
-const a = [10, 20, 30, 40, 50, 60];
-const tar= 50;
+// bubble sort => O(n)2
 
-const bs = (arr, target) => {
-  let index = -1;
-  let start = 0;
-  let end = arr.length - 1;
-  
-//   console.log(start,end,"mid")
-  while (start <= end) {
-    let mid = Math.floor((start + end) / 2);
-    if (arr[mid] === target) {
-      index = mid;
-      break
-    }
+const arr = [10, 8, 20, 5];
 
-    if (target > arr[mid]) {
-      start = mid+1;
-    }
-
-    if (target < arr[mid]) {
-      end = mid-1;
+for (let i = 0; i < arr.length - 1; i++) {
+  let isSwapped = false
+  for (let j = 0; j < arr.length -i- 1; j++) {
+    if (arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      isSwapped= true
     }
   }
 
-  return index;
-};
+  if (!isSwapped) {
+      break
+  }
+}
 
-console.log(bs(a, tar));
+console.log(arr)
