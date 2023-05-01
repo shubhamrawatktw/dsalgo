d801af58e34a68ab57ee22a3d9a3fe07920c852e
shubham <shubham@itday.in>
2023-05-01 10:38:22 +0530

    union of two sorted array

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-// Intersection of two sorted arrays
+// Union of two sorted array 
 
-const a = [3,5,10,10,10,10,15,15,20]
-const b = [5,10,10,15,30]
-const ans =[]
-let i = 0
-let j = 0
+const a = [3,5,8]
+const b = [2,8,9,10,15]
+const res = []
+const finalRes =[]
+let i =0
+let j =0
 let k = 0
 
-while (i < a.length && j<b.length) {
-  console.log("shubham")
-  if (a[i] === b[j]) {
-    ans[k] = a[i]
-    i++,
-    j++,
+while (i < a.length && j < b.length) {
+   
+  if (b[j] < a[i] ) {
+    res[k] = b[j]
+    j++
+    k++
+  }else{
+    res[k] = a[i]
+    i++
     k++
   }
-  if(b[j] > a[i]){
-    i++
-  }
-  if (a[i] > b[j]) {
-    j++
-  }
 }
 
-const sorted =[]
+while (i < a.length) {
+  res[k] = a[i]
+  i++
+  k++
+}
 
-for (let i = 0; i < ans.length; i++) {
-  const element = ans[i];
-  if (element !== ans[i+1]) {
-    sorted.push(element)
-  }
+while (j < b.length) {
+  res[k] = b[j]
+  j++
+  k++
+}
+
+for (let i = 0; i < res.length; i++) {
+ if (res[i] !== res[i+1]) {
+    finalRes.push(res[i])
+ }
   
 }
-console.log(sorted)
+
+console.log(finalRes)
