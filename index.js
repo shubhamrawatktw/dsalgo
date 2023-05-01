5f7a773172e61f788d9c0589e96b427b3cde6747
shubham <shubham@itday.in>
2023-05-01 11:50:27 +0530

    lomuto partittion for quick sort

--- index.js
+++ index.js
@@ -1,43 +1,20 @@
-// Union of two sorted array 
+// Lomuto partition
 
-const a = [3,5,8]
-const b = [2,8,9,10,15]
-const res = []
-const finalRes =[]
-let i =0
-let j =0
-let k = 0
+const arr = [10,80,30,90,40,50,70]
 
-while (i < a.length && j < b.length) {
-   
-  if (b[j] < a[i] ) {
-    res[k] = b[j]
-    j++
-    k++
-  }else{
-    res[k] = a[i]
-    i++
-    k++
-  }
-}
+const pivot = arr[arr.length-1]
 
-while (i < a.length) {
-  res[k] = a[i]
-  i++
-  k++
-}
+let i = -1
+let j = 0
 
-while (j < b.length) {
-  res[k] = b[j]
-  j++
-  k++
-}
-
-for (let i = 0; i < res.length; i++) {
- if (res[i] !== res[i+1]) {
-    finalRes.push(res[i])
- }
+while (j< arr.length-1) {
   
+  if (arr[j] < pivot) {
+    i++
+    [arr[i],arr[j]] = [arr[j],arr[i]]
+  }
+  j++
 }
+[arr[i+1],arr[arr.length-1]] = [arr[arr.length-1],arr[i+1]]
 
-console.log(finalRes)
+console.log(arr)
