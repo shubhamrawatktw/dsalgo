00eb3bedfcde9b919e82aecea499973bb29495ef
shubham <shubham@itday.in>
2023-05-01 12:16:23 +0530

    quick sort --> lumotu partition

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-// Lomuto partition
-
-const arr = [10,80,30,90,40,50,70]
-
-const pivot = arr[arr.length-1]
-
-let i = -1
-let j = 0
-
-while (j< arr.length-1) {
-  
-  if (arr[j] < pivot) {
-    i++
-    [arr[i],arr[j]] = [arr[j],arr[i]]
-  }
-  j++
+// Quick sort --> lomuto partiotion
+function iPartition(arr, l, h)
+{   
+    let pivot=arr[h];
+    let i=l-1;
+    for(let j=l;j<=h-1;j++){
+        if(arr[j]<pivot){
+            i++;
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+    }
+    [arr[i+1], arr[h]] = [arr[h], arr[i+1]];
+    return i+1;
 }
-[arr[i+1],arr[arr.length-1]] = [arr[arr.length-1],arr[i+1]]
 
-console.log(arr)
+function qSort(arr, l, h) {
+    if(l<h){
+        let p=iPartition(arr,l,h);
+        arr = qSort(arr,l,p-1);
+        arr = qSort(arr,p+1,h);
+    }
+    return arr;
+}
+ 
+let arr = [8,4,7,9,3,10,5];
+
+let n = arr.length;
+
+arr = qSort(arr,0,n-1);
+console.log(arr);
