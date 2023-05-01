f996e687c8b317e1dbf75fab931733d23565d8b7
shubham <shubham@itday.in>
2023-05-01 20:45:47 +0530

    snake pattern 2d array

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-// Quick sort --> lomuto partiotion
-function iPartition(arr, l, h)
-{   
-    let pivot=arr[h];
-    let i=l-1;
-    for(let j=l;j<=h-1;j++){
-        if(arr[j]<pivot){
-            i++;
-            [arr[i], arr[j]] = [arr[j], arr[i]];
-        }
-    }
-    [arr[i+1], arr[h]] = [arr[h], arr[i+1]];
-    return i+1;
-}
+// matrix 2D Array 
+const arr = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 
-function qSort(arr, l, h) {
-    if(l<h){
-        let p=iPartition(arr,l,h);
-        arr = qSort(arr,l,p-1);
-        arr = qSort(arr,p+1,h);
-    }
-    return arr;
-}
+for (let i = 0; i < arr.length; i++) {
  
-let arr = [8,4,7,9,3,10,5];
-
-let n = arr.length;
-
-arr = qSort(arr,0,n-1);
-console.log(arr);
+    if (i%2 ===0) {
+        for (let j = 0; j < arr.length; j++) {
+           
+            console.log(arr[i][j])
+        }
+    }else{
+     for (let k = arr.length-1; k >=0; k--) {
+       
+        console.log(arr[i][k])
+     }
+    }
+    
+}
